9790b5537d51c6830ad0f678b9873601c2b23f02
mohith <mohithm2@gmail.com>
2019-09-30 20:48:47 +0530

    Added Admin role

--- TeacherPortal/Startup.cs
+++ TeacherPortal/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TeacherPortal.Models;
 
 [assembly: OwinStartupAttribute(typeof(TeacherPortal.Startup))]
 namespace TeacherPortal
@@ -9,6 +12,47 @@ namespace TeacherPortal
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+        private void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "admin@rnsit.com";
+                user.Email = "admin@rnsit.com";
+                string password = "P@ssw0rd";
+
+                var user1 = userManager.Create(user, password);
+                if (user1.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("faculty"))
+            {
+                var role = new IdentityRole();
+                role.Name = "faculty";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("student"))
+            {
+                var role = new IdentityRole();
+                role.Name = "student";
+                roleManager.Create(role);
+            }
+
         }
     }
 }
