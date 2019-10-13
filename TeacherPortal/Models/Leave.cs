4143da1d5c41a29cf76d7ee0181bb383900bf0f7
AG <anirudh.600@gmail.com>
2019-10-13 17:36:42 +0530

    updated annotations to leave model

--- TeacherPortal/Models/Leave.cs
+++ TeacherPortal/Models/Leave.cs
@@ -11,6 +11,7 @@ namespace TeacherPortal.Models
     public partial class Leave
     {
         [Key]
+        [Display(Name = "ID")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long LeaveID { get; set; }
 
@@ -18,22 +19,29 @@ namespace TeacherPortal.Models
         public string Id { get; set; }
 
         [StringLength(255)]
+        [Display(Name = "Reason")]
         public string LeaveDescription { get; set; }
 
+        [Display(Name = "Temporary Contact")]
         public int? TempContact { get; set; }
 
         [Column(TypeName = "smalldatetime")]
+        [Display(Name = "Start Date")]
         public DateTime? StartDate { get; set; }
 
         [Column(TypeName = "smalldatetime")]
+        [Display(Name = "End Date")]
         public DateTime? EndDate { get; set; }
 
         [StringLength(255)]
+        [Display(Name = "Nature of Leave")]
         public string LeaveType { get; set; }
 
+        [Display(Name = "Leave Count")]
         public short? LeaveCount { get; set; }
 
         [StringLength(255)]
+        [Display(Name = "Approval Status")]
         public string ApprovalStatus { get; set; }
     }
 }
